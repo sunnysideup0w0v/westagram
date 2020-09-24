1dc6b481c17963eba787b925ffc0e54323496ff8
sunnysideup0w0v <723973972538@naver.com>
2020-09-24 20:52:53 +0900

    Add: 프로필 클릭시 프로필박스 생성 - 클릭시 clicked 클래스 생성 및 opacity, visibility 수정(css) - 프로필 박스 css 수정

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ makeList(friendList);
 
 // filter friendList with searching value
 searchInput.addEventListener("keyup", () => {
-  const searchIdList = searchFriend(friendList, searchInput.value);
+  searchFriend(friendList, searchInput.value);
 });
 
 // show friendList when focus on search Input tag
@@ -111,7 +111,7 @@ searchInput.addEventListener("blur", () => {
   searchBox.style.visibility = "hidden";
 });
 
-// comment
+// Make comment
 const registerComment = (comment) => {
   const li = commentList.appendChild(document.createElement("li"));
   li.className = "comment";
@@ -120,19 +120,20 @@ const registerComment = (comment) => {
       <p>${comment}</p>`;
   const likeBtn = li.appendChild(document.createElement("button"));
   likeBtn.classList.add("likeBtn");
+  // likeBtn svg empty heart
   likeBtn.innerHTML = `
-  <svg aria-label="좋아요" class="_8-yf5 " fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
+  <svg fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
   likeBtn.addEventListener("click", () => {
     if (likeBtn.classList.contains("like")) {
-      // console.log("remove");
       likeBtn.classList.remove("like");
+      // likeBtn svg empty heart
       likeBtn.innerHTML = `<svg fill="#262626" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
       return;
     }
 
     if (!likeBtn.classList.contains("like")) {
-      // console.log("add");
       likeBtn.classList.add("like");
+      // likeBtn svg filled heart
       likeBtn.innerHTML = `<svg fill="#ed4956" height="12" viewBox="0 0 48 48" width="12"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>`;
       return;
     }
@@ -158,4 +159,16 @@ registerBtn.addEventListener("click", () => {
   let comment = replyInput.value;
   registerComment(comment);
   replyInput.value = "";
+});
+
+const navProfileButton = document.querySelector(
+  "#nav .navFlexItems .profile_button"
+);
+const profileBox = document.querySelector("#nav .rightContent .profileBox");
+
+navProfileButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  profileBox.classList.contains("clicked")
+    ? profileBox.classList.remove("clicked")
+    : profileBox.classList.add("clicked");
 });
